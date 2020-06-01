5949a2ee43a2c35385448cb94d4bf37fa27a7d07
marko vujic <markoczvvujic@hotmail.com>
2020-06-01 07:36:58 +0200

    work with string

--- 02/demos/after/js-demo/home.js
+++ 02/demos/after/js-demo/home.js
@@ -1,1 +1,7 @@
-showMessage("Changes...")
+let text = "\"Marko\"";
+let message = `Hello ${text}`;
+let number = 345;
+let x = "345.67";
+let amount = Number.parseFloat(x);
+message = message + "!!"+number.toString()+" "+amount;
+showMessage(message);
