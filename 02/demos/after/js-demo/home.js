83c0cefb9d0c7dcaecf06cdd3479a8e879a8006c
marko vujic <markoczvvujic@hotmail.com>
2020-06-01 13:34:35 +0200

    minor changes

--- 02/demos/after/js-demo/home.js
+++ 02/demos/after/js-demo/home.js
@@ -4,4 +4,16 @@ let number = 345;
 let x = "345.67";
 let amount = Number.parseFloat(x);
 message = message + "!!"+number.toString()+" "+amount;
+
+let person = {
+    firstName: "Marko",
+    lastName: "Vujic"
+};
+
+console.log(person);
+
+if(+(1.1+1.3).toFixed(2)===2.4) {
+    console.log("2.4")
+}
+
 showMessage(message);
